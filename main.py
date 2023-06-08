0ba692223a55f0b1ed7ea9d082280a2fd67488f3
IlIllII <78166995+IlIllII@users.noreply.github.com>
2023-06-07 23:47:04 -0400

    Start project

---
+++ main.py
@@ -0,0 +1,155 @@
+import pygame
+from render import render_image, generate_image
+
+dragging = False
+first_point = (0, 0)
+second_point = (0, 0)
+lines = []
+dragging_line = None
+
+
+class Tool:
+    def __init__(self) -> None:
+        pass
+
+    def activate(self):
+        pass
+
+    def deactivate(self):
+        pass
+
+    def draw(self, screen):
+        pass
+
+    def handle_event(self, event):
+        pass
+
+
+class LineTool(Tool):
+    mouse_down = False
+    point1 = (0, 0)
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.mouse_down = False
+        self.point1 = (0, 0)
+
+    def activate(self):
+        self.mouse_down = False
+        self.point1 = (0, 0)
+
+    def deactivate(self):
+        pass
+
+    def handle_event(self, event, canvas):
+        mouse_pos = pygame.mouse.get_pos()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            self.mouse_down = True
+            self.point1 = mouse_pos
+            pygame.draw.line(canvas, (0, 0, 0), self.point1, self.point1, 3)
+            
+        if event.type == pygame.MOUSEMOTION and self.mouse_down:
+            pygame.draw.line(canvas, (0, 0, 0), self.point1, mouse_pos, 3)
+            
+        if event.type == pygame.MOUSEBUTTONUP:
+            self.mouse_down = False
+            pygame.draw.line(canvas, (0, 0, 0), self.point1, mouse_pos, 3)
+
+        return self.mouse_down
+
+
+active_tool = LineTool()
+tools = ["polyline", "eraser", "circle", "rectangle", "fill", "selection", "freehand"]
+
+
+def draw_polyline(screen, points):
+    for i in range(len(points) - 1):
+        pygame.draw.line(screen, (0, 0, 0), points[i], points[i + 1])
+
+
+if __name__ == "__main__":
+    pygame.init()
+    pygame.display.set_caption("Diffusion Sketch!")
+    screen = pygame.display.set_mode((512, 512))
+    temp_canvas = pygame.Surface((512, 512))
+    screen.fill((255, 255, 255))
+    temp_canvas.fill((255, 255, 255))
+    pygame.display.flip()
+    active_tool.activate()
+    screen_copy = screen.copy()
+
+    button_down = False
+    actively_drawing = False
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                actively_drawing = True
+                screen_copy = screen.copy()
+
+            if event.type == pygame.MOUSEBUTTONUP:
+                actively_drawing = False
+                screen.blit(temp_canvas, (0, 0))
+
+            if actively_drawing:
+                screen.blit(screen_copy, (0, 0))
+                temp_canvas.fill((255, 255, 255))
+                temp_canvas.blit(screen_copy, (0, 0))
+
+            actively_drawing = active_tool.handle_event(event, temp_canvas)
+
+            if actively_drawing:
+                screen.blit(temp_canvas, (0, 0))
+            else:
+                screen.blit(screen, (0, 0))
+
+            pygame.display.flip()
+
+            # if event.type == pygame.MOUSEMOTION:
+            #     if dragging:
+            #         second_point = pygame.mouse.get_pos()
+            #         dragging_line = (first_point, second_point)
+            # if event.type == pygame.MOUSEBUTTONDOWN:
+            #     if active_tool is not None:
+            #         active_tool.draw(screen)
+            #     if dragging:
+            #         lines.append(dragging_line)
+            #         dragging = False
+            #     else:
+            #         dragging = True
+            #         first_point = pygame.mouse.get_pos()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_BACKSPACE:
+                    if len(lines) > 0:
+                        lines.pop()
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+                if event.key == pygame.K_s:
+                    pygame.image.save(screen, "screenshot.png")
+                    print("Screenshot saved!")
+                    generate_image()
+
+                if event.key == pygame.K_c:
+                    screen.fill((255, 255, 255))
+                    lines = []
+                    pygame.display.flip()
+
+                if event.key == pygame.K_t:
+                    pygame.draw.circle(screen, (0, 0, 0), pygame.mouse.get_pos(), 10)
+
+        # screen.fill((255, 255, 255))
+
+        # for line in lines:
+        # pygame.draw.line(screen, (0, 0, 0), line[0], line[1], 3)
+
+        # if dragging_line:
+        # pygame.draw.line(screen, (0, 0, 0), dragging_line[0], dragging_line[1])
+
+        # pygame.display.flip()
+
+    pygame.quit()
---
+++ render.py
@@ -0,0 +1,169 @@
+import requests
+from PIL import Image
+from io import BytesIO
+import base64
+import webuiapi
+
+# import json
+
+server = "http://127.0.0.1:7860/sdapi/v1/txt2img"
+
+
+def render_image():
+    start_image = Image.open("screenshot.png")
+    # start_image = start_image.resize((256, 256))
+    start_image = start_image.convert("RGB")
+    buffered = BytesIO()
+    start_image.save(buffered, format="JPEG")
+    start_image = base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+    detection_payload = {
+        "controlnet_module": "pidinet_scribble",
+        "controlnet_input_images": [start_image],
+        "controlnet_processor_res": 512,
+        "controlnet_threshold_a": 64,
+        "controlnet_threshold_b": 64,
+    }
+
+    detection_response = requests.post(
+        url=f"http://127.0.0.1:7860/sdapi/v1/txt2img", json=detection_payload
+    )
+
+    complete_payload = {
+        "enable_hr": "false",
+        "denoising_strength": 0,
+        "firstphase_width": 0,
+        "firstphase_height": 0,
+        "hr_scale": 2,
+        "hr_upscaler": "",
+        "hr_second_pass_steps": 0,
+        "hr_resize_x": 0,
+        "hr_resize_y": 0,
+        "prompt": "An airplane flying in the sky",
+        "styles": [""],
+        "seed": -1,
+        "subseed": -1,
+        "subseed_strength": 0,
+        "seed_resize_from_h": -1,
+        "seed_resize_from_w": -1,
+        "sampler_name": "DDIM",
+        "batch_size": 1,
+        "n_iter": 1,
+        "steps": 20,
+        "cfg_scale": 7,
+        "width": 512,
+        "height": 512,
+        "restore_faces": "false",
+        "tiling": "false",
+        "do_not_save_samples": "false",
+        "do_not_save_grid": "false",
+        "negative_prompt": "bad art",
+        "eta": 0,
+        "s_churn": 0,
+        "s_tmax": 0,
+        "s_tmin": 0,
+        "s_noise": 1,
+        "override_settings": {},
+        "override_settings_restore_afterwards": "true",
+        "script_args": [],
+        "sampler_index": "Euler",
+        "script_name": "",
+        "send_images": "true",
+        "save_images": "false",
+        "alwayson_scripts": {},
+        "controlnet_units": [
+            {
+                "input_image": [start_image],
+                "mask": "",
+                "module": "invert",
+                "model": "controlnet11Models_scribble [4e6af23e]",
+                "weight": 1,
+                "resize_mode": "Inner Fit (Scale to Fit)",
+                "lowvram": "false",
+                "processor_res": 64,
+                "threshold_a": 64,
+                "threshold_b": 64,
+                "guidance": 1,
+                "guidance_start": 0,
+                "guidance_end": 1,
+                "guessmode": "false",
+                "rgbbgr_mode": "false",
+            }
+        ],
+    }
+
+    response = requests.post(
+        url=f"http://127.0.0.1:7860/sdapi/v1/txt2img", json=complete_payload
+    )
+    r = response.json()
+    print(r["info"])
+    print(r["parameters"])
+    for j, i in enumerate(r["images"]):
+        print(f"j: {j}")
+        image2 = Image.open(BytesIO(base64.b64decode(i.split(",", 1)[0])))
+        image2.save(f"output_img2img{j}.png")
+        print(f"output_img2img{j}.png")
+        return i
+
+
+# for i in r["images"]:
+# image = Image.open(BytesIO(base64.b64decode(i.split(",",1)[0])))
+
+
+# im = render_image()
+
+
+controlnet_config = {
+    "prompt": "a castle",
+    "negative_prompt": "(worst quality,low quality,bad quality,normal quality:1.2)",
+    "enable_hr": "false",
+    "seed": 42,
+    "batch_size": 1,
+    "steps": 16,
+    "quick_steps": 12,
+    "cfg_scale": 7,
+    "width": 512,
+    "height": 512,
+    "override_settings": {"CLIP_stop_at_last_layers": 1},
+    "override_settings_restore_afterwards": "true",
+    "controlnet_units": [
+        {
+            "weight": 0.6,
+            "guidance_start": 0.0,
+            "guidance_end": 1.0,
+            "module": "none",
+            "pixel_perfect": "false",
+        }
+    ],
+}
+
+
+
+def generate_image():
+    api = webuiapi.WebUIApi()
+    img = Image.open("screenshot.png")
+    scribble_unit = webuiapi.ControlNetUnit(
+        input_image=img,
+        module="invert",
+        model="controlnet11Models_scribble [4e6af23e]",
+        weight=1,
+    )
+
+    # unit1 = webuiapi.ControlNetUnit(input_image=img, module='canny', model='controlnet11Models_canny [b18e0966]')
+
+    
+    
+    r = api.img2img(
+        images=[img],
+        prompt="airplane flying through the clouds, photorealistic, 8k, absurdres",
+        negative_prompt="bad art",
+        controlnet_units=[scribble_unit],
+        sampler_name="DPM++ 2M Karras",
+        cfg_scale=7,
+        steps=40
+    )
+    r.images[0].save("output.png")
+    # r.image.show()
+    # r.images[1].show()
+
+# generate_image()
